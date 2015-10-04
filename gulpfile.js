244bc425535c419697420d2aa4bbc99e39f65a75
Ryan Sandor Richards <sandor.richards@gmail.com>
2015-10-04 04:26:48 -0700

    Added basic watch tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var order = require('gulp-order');
  * Common directories used by tasks below.
  * @type {object}
  */
-var dirs = {
+var path = {
   source: {
     coffee: 'src/',
     sass: 'sass/'
@@ -37,11 +37,19 @@ var dirs = {
 gulp.task('default', ['build']);
 
 /**
+ * Watch script for recompiling JavaScript and CSS
+ */
+gulp.task('watch', function () {
+  gulp.watch(path.source.coffee + '**/*.coffee', ['build']);
+  gulp.watch(path.source.sass + '**/*.scss', ['sass', 'sass-minify']);
+});
+
+/**
  * Runs mocha unit tests for the project. This can also be initiated by calling
  * `npm test` (which simply calls into this gulp task).
  */
 gulp.task('unit', ['build'], function () {
-  gulp.src(dirs.test.unit + '**/*.coffee')
+  gulp.src(path.test.unit + '**/*.coffee')
     .pipe(mocha({ reporter: 'dot' }));
 });
 
@@ -49,7 +57,7 @@ gulp.task('unit', ['build'], function () {
  * Cleans all build and distribution files.
  */
 gulp.task('clean', function (cb) {
-  del([ dirs.dist.js, dirs.dist.css]).then(function () {
+  del([ path.dist.js, path.dist.css]).then(function () {
     cb();
   });
 });
@@ -58,20 +66,20 @@ gulp.task('clean', function (cb) {
  * Generates epoch CSS from Sass source.
  */
 gulp.task('sass', function () {
-  gulp.src(dirs.source.sass + 'epoch.scss')
+  gulp.src(path.source.sass + 'epoch.scss')
     .pipe(sass({ outputStyle: 'compact' }))
     .pipe(rename('epoch.css'))
-    .pipe(gulp.dest(dirs.dist.css));
+    .pipe(gulp.dest(path.dist.css));
 });
 
 /**
  * Generates the minified version of the epoch css from sass source.
  */
 gulp.task('sass-minify', function () {
-  gulp.src(dirs.source.sass + 'epoch.scss')
+  gulp.src(path.source.sass + 'epoch.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(rename('epoch.min.css'))
-    .pipe(gulp.dest(dirs.dist.css));
+    .pipe(gulp.dest(path.dist.css));
 });
 
 /**
@@ -80,7 +88,7 @@ gulp.task('sass-minify', function () {
  * directory
  */
 gulp.task('build', ['sass', 'sass-minify'], function () {
-  gulp.src(dirs.source.coffee + '**/*.coffee')
+  gulp.src(path.source.coffee + '**/*.coffee')
     .pipe(coffee({bare: true}).on('error', gutil.log))
     .pipe(order([
       'epoch.js',
@@ -100,8 +108,8 @@ gulp.task('build', ['sass', 'sass-minify'], function () {
       'adapters/*.js'
     ]))
     .pipe(concat('epoch.js'))
-    .pipe(gulp.dest(dirs.dist.js))
+    .pipe(gulp.dest(path.dist.js))
     .pipe(uglify().on('error', gutil.log))
     .pipe(rename('epoch.min.js'))
-    .pipe(gulp.dest(dirs.dist.js));
+    .pipe(gulp.dest(path.dist.js));
 });
