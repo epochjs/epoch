abfd19ffe5bfba64caa909f94b8f598bd1d889d2
Ryan Sandor Richards <sandor.richards@gmail.com>
2015-10-04 04:41:37 -0700

    Added doc generator task.

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@ require('coffee-script/register'); // For coffee-script mocha unit tests
 var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
-var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
+var mocha = require('gulp-mocha');
+var order = require('gulp-order');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
-var mocha = require('gulp-mocha');
+var uglify = require('gulp-uglify');
+var gutil = require('gulp-util');
 var del = require('del');
-var order = require('gulp-order');
+var exec = require('child_process').exec;
 
 /**
  * Common directories used by tasks below.
@@ -28,59 +29,14 @@ var path = {
   },
   test: {
     unit: 'tests/unit/'
-  }
+  },
+  doc: 'doc/'
 };
 
 /**
  * The default task simply calls the master 'build' task.
  */
 gulp.task('default', ['build']);
-
-/**
- * Watch script for recompiling JavaScript and CSS
- */
-gulp.task('watch', function () {
-  gulp.watch(path.source.coffee + '**/*.coffee', ['build']);
-  gulp.watch(path.source.sass + '**/*.scss', ['sass', 'sass-minify']);
-});
-
-/**
- * Runs mocha unit tests for the project. This can also be initiated by calling
- * `npm test` (which simply calls into this gulp task).
- */
-gulp.task('unit', ['build'], function () {
-  gulp.src(path.test.unit + '**/*.coffee')
-    .pipe(mocha({ reporter: 'dot' }));
-});
-
-/**
- * Cleans all build and distribution files.
- */
-gulp.task('clean', function (cb) {
-  del([ path.dist.js, path.dist.css]).then(function () {
-    cb();
-  });
-});
-
-/**
- * Generates epoch CSS from Sass source.
- */
-gulp.task('sass', function () {
-  gulp.src(path.source.sass + 'epoch.scss')
-    .pipe(sass({ outputStyle: 'compact' }))
-    .pipe(rename('epoch.css'))
-    .pipe(gulp.dest(path.dist.css));
-});
-
-/**
- * Generates the minified version of the epoch css from sass source.
- */
-gulp.task('sass-minify', function () {
-  gulp.src(path.source.sass + 'epoch.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(rename('epoch.min.css'))
-    .pipe(gulp.dest(path.dist.css));
-});
 
 /**
  * Builds the distribution files by packaging the compiled javascript source
@@ -112,4 +68,62 @@ gulp.task('build', ['sass', 'sass-minify'], function () {
     .pipe(uglify().on('error', gutil.log))
     .pipe(rename('epoch.min.js'))
     .pipe(gulp.dest(path.dist.js));
+});
+
+/**
+ * Generates epoch CSS from Sass source.
+ */
+gulp.task('sass', function () {
+  gulp.src(path.source.sass + 'epoch.scss')
+    .pipe(sass({ outputStyle: 'compact' }))
+    .pipe(rename('epoch.css'))
+    .pipe(gulp.dest(path.dist.css));
+});
+
+/**
+ * Generates the minified version of the epoch css from sass source.
+ */
+gulp.task('sass-minify', function () {
+  gulp.src(path.source.sass + 'epoch.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(rename('epoch.min.css'))
+    .pipe(gulp.dest(path.dist.css));
+});
+
+/**
+ * Watch script for recompiling JavaScript and CSS
+ */
+gulp.task('watch', function () {
+  gulp.watch(path.source.coffee + '**/*.coffee', ['build']);
+  gulp.watch(path.source.sass + '**/*.scss', ['sass', 'sass-minify']);
+});
+
+/**
+ * Runs mocha unit tests for the project. This can also be initiated by calling
+ * `npm test` (which simply calls into this gulp task).
+ */
+gulp.task('unit', ['build'], function () {
+  gulp.src(path.test.unit + '**/*.coffee')
+    .pipe(mocha({ reporter: 'dot' }));
+});
+
+/**
+ * Cleans all build and distribution files.
+ */
+gulp.task('clean', function (cb) {
+  del([ path.dist.js, path.dist.css]).then(function () {
+    cb();
+  });
+});
+
+/**
+ * Generates codo documentation from the project's coffeescript source.
+ */
+gulp.task('doc', function (cb) {
+  var command = [
+    './node_modules/.bin/codo --quiet --private',
+    '--name Epoch --readme README.md --title "Epoch Documentation"',
+    '--output #{dirs.doc} #{dirs.src} - LICENSE'
+  ].join('');
+  exec(command, cb);
 });
